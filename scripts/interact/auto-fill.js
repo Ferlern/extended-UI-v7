300e9ad24084a314a1c6459df9c2533776a75c64
lifestarse <57958388+lifestarse@users.noreply.github.com>
2023-04-09 18:19:50 +0300

    Update auto-fill.js

--- scripts/interact/auto-fill.js
+++ scripts/interact/auto-fill.js
@@ -36,7 +36,11 @@ Events.run(Trigger.update, () => {
 
     if (stack.amount) {
         Call.transferInventory(player, core);
-    } else {
+    } 
+    if (stack.amount > 0) {
+            Call.dropItem(0);
+        }
+    else {
         Call.requestItem(player, core, request, 999);
     }
     timer.increase();
